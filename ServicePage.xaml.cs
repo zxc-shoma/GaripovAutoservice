acabfb54d56d3f2f84bf043e340ea675489f98f6
shoma <zlonamerenniy@yandex.ru>
2023-10-17 14:15:25 +0530

    7 лаба

--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -20,17 +20,117 @@ namespace GaripovAutoservice
     /// </summary>
     public partial class ServicePage : Page
     {
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+
+        List<Service> CurrentPageList = new List<Service>();
+        List<Service> TableList;
         public ServicePage()
         {
             InitializeComponent();
             var currentServices = garipov_autoserviceEntities.GetContext().Service.ToList();
+            UpdateServices();
 
             ServiceListView.ItemsSource = currentServices;
             ComboType.SelectedIndex = 0;
 
-            UpdateServices();
+            
         }
+        private void ChangePage(int direction, int? selectedPage)
+        {
 
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+
+            if (CountRecords%10>0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+
+            Boolean Ifupdate = true;
+
+            int min;
+
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage>=0 && selectedPage <=CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i= CurrentPage*10;i<min;i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch(direction)
+                {
+                    case 1:
+                        
+                            if (CurrentPage>0)
+                            {
+
+                            
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i=CurrentPage *10;i<min;i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+
+                    case 2:
+                        if (CurrentPage<CountPage-1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i=CurrentPage*10;i<min;i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+
+                        break;
+
+                }
+            }
+
+            if (Ifupdate)
+            {
+                PageListBox.Items.Clear();
+                for (int i=1;i<=CountPage;i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+
+                min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                TBCount.Text = min.ToString();
+                TBAllRecords.Text = " из " + CountRecords.ToString();
+                ServiceListView.ItemsSource = CurrentPageList;
+                ServiceListView.Items.Refresh();
+                    
+            }
+
+
+        }
         private void UpdateServices()
         {
             var currentServices = garipov_autoserviceEntities.GetContext().Service.ToList();
@@ -73,6 +173,10 @@ namespace GaripovAutoservice
             }
 
             ServiceListView.ItemsSource = currentServices;
+
+            TableList = currentServices;
+
+            ChangePage(0, 0);
 
 
 
@@ -120,8 +224,63 @@ namespace GaripovAutoservice
             {
                 garipov_autoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
                 ServiceListView.ItemsSource = garipov_autoserviceEntities.GetContext().Service.ToList();
-
+                UpdateServices();
             }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentService = (sender as Button).DataContext as Service;
+            var currentClientServices = garipov_autoserviceEntities.GetContext().ClientService.ToList();
+            currentClientServices = currentClientServices.Where(p => p.ServiceID == currentService.ID).ToList();
+
+            if (currentClientServices.Count != 0)
+                MessageBox.Show("Невозможно выполнить удаление, так как существуют записи на эту услугу");
+            else
+            {
+
+
+
+
+
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+
+                    try
+                    {
+                        garipov_autoserviceEntities.GetContext().Service.Remove(currentService);
+                        garipov_autoserviceEntities.GetContext().SaveChanges();
+
+                        ServiceListView.ItemsSource = garipov_autoserviceEntities.GetContext().Service.ToList();
+
+                        UpdateServices();
+
+                    }
+
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+                
+
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
         }
     }
 }
